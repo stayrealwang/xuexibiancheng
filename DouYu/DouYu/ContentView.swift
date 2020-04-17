c0634b6aecc4287d668d27e8f414d38423a3a833
stayrealwang <stayrealwang@icloud.com>
2020-04-17 15:18:38 +0800

    初始化信息

---
+++ DouYu/DouYu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DouYu
+//
+//  Created by 王均达 on 2020/4/17.
+//  Copyright © 2020 王均达. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
